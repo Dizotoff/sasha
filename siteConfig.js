52c503f26f72424238d8de0038a594d5f111cd40
Dmytro Izotov <izotov.dima@gmail.com>
2021-03-23 23:03:11 +0200

    Intermediate commit

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: 'London After Midnight',
-  shortName: 'LAM',
-  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
-  url: 'https://gatsby-lam.vaporwavy.io', // Domain of your site without prefix!
+  name: 'DURÁ',
+  shortName: 'DURÁ',
+  description: 'Photographer portfolio website',
+  url: 'https://savelievvaa.com', 
   prefix: '/',
-  author: 'Vaporwavy',
+  author: '',
   twitter: ''
 }
